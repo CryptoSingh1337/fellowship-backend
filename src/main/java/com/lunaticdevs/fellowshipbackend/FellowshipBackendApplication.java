3d591d0f995e0e678aa30349d4aaacbb97bb105d
Saransh Kumar <cryptosingh@gmail.com>
2022-07-31 11:52:08 +0530

    Initial commit

---
+++ src/main/java/com/lunaticdevs/fellowshipbackend/FellowshipBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lunaticdevs.fellowshipbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FellowshipBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FellowshipBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lunaticdevs/fellowshipbackend/FellowshipBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lunaticdevs.fellowshipbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FellowshipBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
