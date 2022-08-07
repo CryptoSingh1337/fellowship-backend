9c30adb3bafd37dcda5d1cd33e23ff521d19ea7f
Saransh Kumar <cryptosingh@gmail.com>
2022-08-07 17:06:31 +0530

    Create cors config

---
+++ FellowshipSecurity/src/main/java/com/cyborg/fellowshipsecurity/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.cyborg.fellowshipsecurity.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author saranshk04
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "https://fellowship-frontend-six.vercel.app")
+                .allowedMethods("GET", "POST", "DELETE", "PUT");
+    }
+}
